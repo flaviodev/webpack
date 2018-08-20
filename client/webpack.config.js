4cccc06677d11b7ba07b8bc6f7b369a7dafd9f2d
flavio <fdsdev@gmail.com>
2018-08-20 06:46:55 -0300

    finalizando aula 2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
-
 let plugins = [];
 
 if(process.env.NODE_ENV == 'production') {
+
     plugins.push(new babiliPlugin());
 }
 
